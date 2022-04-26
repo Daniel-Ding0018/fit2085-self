38eb748225ee3472d16c820029bfdf199cc0ad9f
Daniel Ding <ddin0018@student.monash.edu>
2022-04-26 14:56:42 +1000

    change

--- pokemon_base.py
+++ pokemon_base.py
@@ -3,3 +3,10 @@ class PokemonBase:
         self.hp = hp
         self.poke_type = poke_type
         self.level = 1
+
+
+    def set_hp(self,hp: int) -> None:
+        self.hp = hp
+
+    def get_hp(self) -> int:
+        return self.hp
